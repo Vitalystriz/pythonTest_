142e629f9af68e5b442481088b4840d03f41693a
vitalystriz <vitalstri@ya.ru>
2021-12-03 00:32:23 +0300

    7

--- main.py
+++ main.py
@@ -15,9 +15,12 @@ if __name__ == '__main__':
 import sqlite3
 import telebot
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+token = "2087683356:AAGZgp67CrWWVIij1iv-rpMyKTcG6KM5es0"
+bot = telebot.TeleBot(token)
 
 contactNumber=["A-79345657","B-7898403","C-7645453"] ## сначала имя потом номер
 note=["A","B","w"]
+
 connection=sqlite3.connect("contacts.db")
 cursor=connection.cursor()
 cursor.execute("""CREATE TABLE IF NOT EXISTS contacts(
@@ -25,6 +28,7 @@ name TEXT,
 contactNumber TEXT)
 """)
 connection.commit()
+
 class PhoneBook ():
     def __init__(self, contactNumber, note):
         pass
@@ -36,10 +40,55 @@ class PhoneBook ():
                     local_cursor = local_connection.cursor()
                     local_cursor.execute(f"INSERT INTO contacts VALUES(?,?);",(contactNumber[i][:j],contactNumber[i][j+1:]))
                     local_connection.commit()
+@bot.message_handler(commands=["start"])
+def send_start_message(message):
+    bot.send_message(message.chat.id, "формат 'имя-0000000'")
+@bot.message_handler(content_types=["text"])
+def send_start_message(message):
+    to_chat_id=message.chat.id
+    msg=message.text
+    if msg.isalpha()==True:
+
+        local_connection = sqlite3.connect("contacts.db")
+        local_cursor = local_connection.cursor()
+        local_cursor.execute(f" SELECT * from contacts ;")
+        all_fetchall = local_cursor.fetchall()
+        for i in range(len(all_fetchall)):
+            for j in range(len(all_fetchall[i])):
+                if str(all_fetchall[i][j])==msg:
+                    number=all_fetchall[i][j+1]
+
+        bot.send_message(to_chat_id,number)
+    if msg.isdigit() == True:
+
+        local_connection = sqlite3.connect("contacts.db")
+        local_cursor = local_connection.cursor()
+        local_cursor.execute(f" SELECT * from contacts ;")
+        all_fetchall = local_cursor.fetchall()
+        for i in range(len(all_fetchall)):
+            for j in range(len(all_fetchall[i])):
+                if str(all_fetchall[i][j]) == msg:
+                    number = all_fetchall[i][j - 1]
+
+        bot.send_message(to_chat_id, number)
+    if msg.isdigit() != True and msg.isalpha != True:
+        for j in range(len(msg)):
+            if msg[j] == "-":
+                local_connection = sqlite3.connect("contacts.db")
+                local_cursor = local_connection.cursor()
+                local_cursor.execute(f"INSERT INTO contacts VALUES(?,?);",
+                                     (msg[:j], msg[j + 1:]))
+                local_connection.commit()
+
+
+
+
+        bot.send_message(to_chat_id, number)
+
 
 
 
 
 PhoneContact1=PhoneBook(contactNumber, note)
 
-
+bot.polling(none_stop=True, interval=0)
