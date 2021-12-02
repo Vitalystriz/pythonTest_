3c18af8b2e48cbb91d3cb7fd81a02f0c6e7cc688
vitalystriz <vitalstri@ya.ru>
2021-12-03 00:02:10 +0300

    4

--- main.py
+++ main.py
@@ -12,26 +12,32 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+import sqlite3
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-name="A"
+
 contactNumber=["A-79345657","B-7898403","C-7645453"] ## сначала имя потом номер
 note=["A","B","w"]
-
+connection=sqlite3.connect("contacts.db")
+cursor=connection.cursor()
+cursor.execute("""CREATE TABLE IF NOT EXISTS contacts(
+name TEXT,
+contactNumber TEXT)
+""")
 class PhoneBook ():
-    def __init__(self,name, contactNumber, note):
+    def __init__(self, contactNumber, note):
         pass
-    def getContactByName(self,contactNumber,name):
+    def getContactByName(self,contactNumber):
         for i in range(len(contactNumber)):
             for j in range (len(contactNumber[i])):
                 if contactNumber[i][j]=="-":
-                    if contactNumber[i][:j]==name:
-                        print(contactNumber[i][j+1:])
-                        return contactNumber[i][j+1:]
-                    else:
-                        raise ValueError("Формат ввода'имя-00000000'")
+                    local_connection = sqlite3.connect("contacts.db")
+                    local_cursor = local_connection.cursor()
+                    local_cursor.execute(f"INSERT INTO contacts VALUES(?,?);",(contactNumber[i][:j],contactNumber[i][j+1:]))
+                    local_connection.commit()
 
 
-PhoneContact1=PhoneBook(name, contactNumber, note)
 
-print(PhoneContact1.getContactByName(contactNumber,name))
+
+PhoneContact1=PhoneBook(contactNumber, note)
+
+
